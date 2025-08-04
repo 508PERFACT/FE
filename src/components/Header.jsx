c01c4aec20b07dcfd7c2c14c113e113326aba95d
구혁모 <gurah129@gmail.com>
2025-08-05 03:42:47 +0900

    ✨: Header 마크업 + 스타일 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import styles from '@/styles/components/Header.module.scss';
+import { logo_header, logo_title } from '@/assets';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.blank}></div>
+        <div className={styles.logoTitle}>
+          <img src={logo_title} alt="title" />
+        </div>
+        <div className={styles.rightWrapper}>
+          <div className={styles.planButton}>
+            <span>구독 플랜</span>
+          </div>
+          <div className={styles.logoIcon}>
+            <img src={logo_header} alt="logo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
