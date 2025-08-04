c700135abea3934c23a607f56a181d15b331b766
구혁모 <gurah129@gmail.com>
2025-08-05 03:06:05 +0900

    ✨: Layout, Header 컴포넌트 생성 및 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChatBotPage } from './page/ChatBotPage';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/chatbot" element={<ChatBotPage />} />
+        <Route element={<Layout />}>
+          <Route path="/chatbot" element={<ChatBotPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
