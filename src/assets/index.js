88b6a305a2cd9910df9c3fe9653acdc95716f43f
구혁모 <gurah129@gmail.com>
2025-08-06 01:14:30 +0900

    🍱: 레포트 저장함 페이지 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,15 @@ import logo_header from './logo.svg';
 import logo_chatbot from './chat-icon.svg';
 import star_sub_white from './star-sub-icon-white.svg';
 import report_white from './report-icon-white.svg';
+import report_blue from './report-icon-blue.svg';
+import arrow_right from './arrow-right.svg';
 
-export { logo_title, logo_header, logo_chatbot, star_sub_white, report_white };
+export {
+  logo_title,
+  logo_header,
+  logo_chatbot,
+  star_sub_white,
+  report_white,
+  report_blue,
+  arrow_right,
+};
