331284fbafdbf17e733206d715f38b0692998336
구혁모 <gurah129@gmail.com>
2025-08-05 22:55:05 +0900

    🍱: 마이페이지 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import logo_title from './title.svg';
 import logo_header from './logo.svg';
 import logo_chatbot from './chat-icon.svg';
+import star_sub_white from './star-sub-icon-white.svg';
+import report_white from './report-icon-white.svg';
 
-export { logo_title, logo_header, logo_chatbot };
+export { logo_title, logo_header, logo_chatbot, star_sub_white, report_white };
