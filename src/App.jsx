44ff5716efaf3fbf6ddefc597f9d7e05e72c1927
nyun-nye <2001_11_7@naver.com>
2025-08-05 18:40:15 +0900

    ✨: 로그인 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChatBotPage } from './page/ChatBotPage';
 import { Layout } from './components/Layout';
+import { Login } from './page/Login';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/chatbot" element={<ChatBotPage />} />
+        </Route>
+        <Route>
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
