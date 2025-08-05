5710fdf3419f26a7161c38b88ff87d607dae7d03
구혁모 <gurah129@gmail.com>
2025-08-06 03:11:59 +0900

    🍱: 구독 페이지 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,7 @@ import logo_title from './title.svg';
 import logo_header from './logo.svg';
 import logo_chatbot from './chat-icon.svg';
 import star_sub_white from './star-sub-icon-white.svg';
+import star_sub_blue from './star-sub-icon-blue.svg';
 import report_white from './report-icon-white.svg';
 import report_blue from './report-icon-blue.svg';
 import arrow_right from './arrow-right.svg';
@@ -11,6 +12,7 @@ export {
   logo_header,
   logo_chatbot,
   star_sub_white,
+  star_sub_blue,
   report_white,
   report_blue,
   arrow_right,
