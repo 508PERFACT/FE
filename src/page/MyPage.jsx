8efc1d1b50738667da5b63f48146a58062c9ad04
구혁모 <gurah129@gmail.com>
2025-08-09 01:13:37 +0900

    ✨: 마이 페이지 링크 연결 및 로그아웃 확인 모달 추가

--- src/page/MyPage.jsx
+++ src/page/MyPage.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/styles/pages/MyPage.module.scss';
 import { logo_header, report_white, star_sub_white } from '@/assets';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const MyPage = () => {
+  const navigate = useNavigate();
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+
+  const handleLogout = () => {
+    console.log('logout');
+    setIsLogoutModalOpen(false);
+    navigate('/login');
+  };
   return (
     <div className={styles.container}>
       <div className={styles.profileContent}>
-        <span className={styles.homeLink}>&larr; 홈페이지</span>
+        <Link className={styles.homeLink} to={'/'}>
+          &larr; 홈페이지
+        </Link>
         <div className={styles.profileLogo}>
           <img src={logo_header} alt="" style={{ width: '80px' }} />
         </div>
         <div className={styles.textWrapper}>
           <div className={styles.title}>
-            안녕하세요 <span>화이팅</span>님
+            안녕하세요 <span>{'화이팅'}</span>님
           </div>
           <div className={styles.subTitle}>
             {'화이팅'}님을 위해 열심히 문서들의 팩트를 체크하고 있어요!
@@ -22,7 +33,10 @@ export const MyPage = () => {
       <div className={styles.divider}></div>
       <div className={styles.buttonContent}>
         <div className={styles.buttonWrapper}>
-          <div className={styles.buttonTextWrapper}>
+          <div
+            className={styles.buttonTextWrapper}
+            onClick={() => navigate(`/myreports/1`)}
+          >
             <div className={styles.buttonTitle}>레포트 저장함</div>
             <div className={styles.buttonDesc}>
               저장한 레포트들을
@@ -35,7 +49,10 @@ export const MyPage = () => {
           </div>
         </div>
         <div className={styles.buttonWrapper}>
-          <div className={styles.buttonTextWrapper}>
+          <div
+            className={styles.buttonTextWrapper}
+            onClick={() => navigate(`/subscribe`)}
+          >
             <div className={styles.buttonTitle}>구독 상태 확인</div>
           </div>
           <div className={styles.buttonImg}>
@@ -43,7 +60,32 @@ export const MyPage = () => {
           </div>
         </div>
       </div>
-      <span className={styles.logout}>로그아웃</span>
+      <span
+        className={styles.logout}
+        onClick={() => setIsLogoutModalOpen(true)}
+      >
+        로그아웃
+      </span>
+      {isLogoutModalOpen && (
+        <div className={styles.modalWrapper}>
+          <div className={styles.modalContent}>
+            <div className={styles.modalDesc}>
+              <span className={styles.modalTitle}>로그아웃 하시겠습니까?</span>
+            </div>
+            <div className={styles.modalButtonWrapper}>
+              <div className={styles.cancelButton} onClick={handleLogout}>
+                로그아웃
+              </div>
+              <div
+                className={styles.confirmButton}
+                onClick={() => setIsLogoutModalOpen(false)}
+              >
+                뒤로가기
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
