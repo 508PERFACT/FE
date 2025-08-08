d152be343ae9a1717a0046ec8bd748f2dd2af7fc
구혁모 <gurah129@gmail.com>
2025-08-09 02:02:43 +0900

    ✨: 레포트 저장함 통신 및 UX 로직 추가 + 명세서에 맞는 목데이터로 수정

--- src/page/MyReports.jsx
+++ src/page/MyReports.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
 import styles from '@/styles/pages/MyReports.module.scss';
 import { arrow_right, report_blue } from '@/assets';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import api from '@/apis/axiosInstance';
 
 export const MyReports = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [pageData, setPageData] = useState();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const currentPage = Number(id);
+
+  useEffect(() => {
+    const getReportsList = async () => {
+      try {
+        // const res = await api.get(`users?page=${currentPage}`);
+        // setList(res.data.result);
+        setPageData(reportsData.result); // 테스트용
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getReportsList();
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    navigate(`/myreports/${page}`);
+  };
+
+  const formattedDate = (raw) => {
+    const date = new Date(raw);
+    return date.toLocaleString('ko-KR', {
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false, // 24시간제
+    });
+  };
 
   return (
     <div className={styles.container}>
@@ -12,25 +45,42 @@ export const MyReports = () => {
         <span>레포트 저장함</span>
       </div>
       <div className={styles.content}>
-        {reportsData.map((report) => (
-          <div className={styles.listItem} key={report.id}>
+        {pageData?.reports.map((report) => (
+          <div className={styles.listItem} key={report.reportId}>
             <div className={styles.listLeft}>
-              <div className={styles.date}>{report.date}</div>
-              <div className={styles.listTitle}>{report.title}</div>
+              <div className={styles.date}>
+                {formattedDate(report.createdAt)}
+              </div>
+              <div
+                className={styles.listTitle}
+                onClick={() => navigate(`/report/${report.reportId}`)}
+              >
+                {report.title}
+              </div>
             </div>
-            <img src={arrow_right} alt="arrow" />
+            <img
+              src={arrow_right}
+              alt="arrow"
+              onClick={() => navigate(`/report/${report.reportId}`)}
+            />
           </div>
         ))}
       </div>
       <div className={styles.PageNav}>
-        {currentPage !== 1 && (
-          <div className={styles.prevButton}>
+        {currentPage != 1 && (
+          <div
+            className={styles.prevButton}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
             <img src={arrow_right} alt="arrow" />
           </div>
         )}
-        <div className={styles.currentPage}>{currentPage}</div>
-        {currentPage <= 10 - 1 && (
-          <div className={styles.nextButton}>
+        <div className={styles.currentPage}>{id}</div>
+        {(pageData?.last || pageData?.totalPages > currentPage) && (
+          <div
+            className={styles.nextButton}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
             <img src={arrow_right} alt="arrow" />
           </div>
         )}
@@ -39,37 +89,50 @@ export const MyReports = () => {
   );
 };
 
-const reportsData = [
-  {
-    id: 1,
-    date: '7월 26일 12:09',
-    title:
-      '“소비쿠폰 대신 이거 써유”… 홍성 주민, 난민·외국인에 심시일반 전한 ‘마음’',
+const reportsData = {
+  isSuccess: true,
+  code: 'SUCCESS',
+  message: '데이터 조회 성공',
+  result: {
+    reports: [
+      {
+        reportId: 1,
+        title:
+          '“소비쿠폰 대신 이거 써유”… 홍성 주민, 난민·외국인에 심시일반 전한 ‘마음’',
+        createdAt: '2025-07-26T12:09:00',
+      },
+      {
+        reportId: 2,
+        title:
+          '[뉴스토리] “정상 체중도 맞는다”…기저의 비만약 ‘위고비’, 오남용 우려도',
+        createdAt: '2025-07-27T13:00:00',
+      },
+      {
+        reportId: 3,
+        title:
+          '휴가철 홍역 환자 “급증”…“예방접종으로 안전한 해외여행 즐기세요”',
+        createdAt: '2025-07-26T16:09:00',
+      },
+      {
+        reportId: 4,
+        title: '고령 운전사고 늘지만 면허증 반납만이 능사아냐',
+        createdAt: '2025-07-26T22:09:00',
+      },
+      {
+        reportId: 5,
+        title:
+          '휴가철 홍역 환자 “급증”…“예방접종으로 안전한 해외여행 즐기세요”',
+        createdAt: '2025-07-26T16:09:00',
+      },
+      {
+        reportId: 6,
+        title: '고령 운전사고 늘지만 면허증 반납만이 능사아냐',
+        createdAt: '2025-07-26T22:09:00',
+      },
+    ],
+    currentPage: 1,
+    totalPages: 3,
+    totalElements: 18,
+    last: false,
   },
-  {
-    id: 2,
-    date: '7월 27일 13:00',
-    title:
-      '[뉴스토리] “정상 체중도 맞는다”…기저의 비만약 ‘위고비’, 오남용 우려도',
-  },
-  {
-    id: 3,
-    date: '7월 26일 16:09',
-    title: '휴가철 홍역 환자 “급증”…“예방접종으로 안전한 해외여행 즐기세요”',
-  },
-  {
-    id: 4,
-    date: '7월 26일 22:09',
-    title: '고령 운전사고 늘지만 면허증 반납만이 능사아냐',
-  },
-  {
-    id: 5,
-    date: '7월 26일 16:09',
-    title: '휴가철 홍역 환자 “급증”…“예방접종으로 안전한 해외여행 즐기세요”',
-  },
-  {
-    id: 6,
-    date: '7월 26일 22:09',
-    title: '고령 운전사고 늘지만 면허증 반납만이 능사아냐',
-  },
-];
+};
