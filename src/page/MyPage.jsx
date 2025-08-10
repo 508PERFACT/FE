a674444778be136b0900d50124a40d13941415e0
nyun-nye <2001_11_7@naver.com>
2025-08-10 14:29:22 +0900

    🚑: 마이페이지 로그아웃시 모달 스크롤 상단으로 이동

--- src/page/MyPage.jsx
+++ src/page/MyPage.jsx
@@ -26,7 +26,23 @@ export const MyPage = () => {
       setIsLogoutModalOpen(false);
     }
   };
+  // 모달이 열릴 때 스크롤 제어
+  useEffect(() => {
+    if (isLogoutModalOpen) {
+      // 스크롤을 맨 위로 이동
+      window.scrollTo(0, 0);
+      // 스크롤 비활성화
+      document.body.style.overflow = 'hidden';
+    } else {
+      // 스크롤 활성화
+      document.body.style.overflow = 'auto';
+    }
 
+    // 컴포넌트 언마운트 시 스크롤 복원
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isLogoutModalOpen]);
   useEffect(() => {
     const fetchNickname = async () => {
       const res = await api.get('users/nickname');
