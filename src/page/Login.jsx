9e3ebb789db4ef1ae7cd0f1f0cde9e264f87de39
구혁모 <gurah129@gmail.com>
2025-08-19 01:06:37 +0900

    💄: 로그인 페이지 피그마 수정 디자인 반영

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -11,26 +11,31 @@ export const Login = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.mainImg}>
-          <img src={main_img} alt="main" />
-        </div>
-        <div className={styles.logo}>
-          <img src={logo_title} alt="logo" />
-        </div>
-        <div className={styles.titleContainer}>
-          <div className={styles.title}>Hello. welcome.</div>
-          <div className={styles.subTitle}>
-            완벽하게 정제된 진실을 전하는 AI 정보 큐레이터 퍼팩트
+        <div className={styles.leftContent}>
+          <div className={styles.logo}>
+            <img src={logo_title} alt="logo" />
+          </div>
+          <div className={styles.titleContainer}>
+            <div className={styles.title}>Hello. welcome.</div>
+            <div className={styles.subTitle}>
+              완벽하게 정제된 진실을 전하는 AI 정보 큐레이터 퍼팩트
+            </div>
+          </div>
+          <div className={styles.button}>
+            <button className={styles.serviceButton}>서비스 소개</button>
+            <button className={styles.naverButton} onClick={loginWithNaver}>
+              네이버 로그인
+            </button>
+            <div className={styles.divider}>
+              <span>또는</span>
+            </div>
+            <button className={styles.guestButton} onClick={handleGuest}>
+              게스트 로그인
+            </button>
           </div>
         </div>
-        <div className={styles.button}>
-          <button className={styles.serviceButton}>서비스 소개</button>
-          <button className={styles.naverButton} onClick={loginWithNaver}>
-            네이버 로그인
-          </button>
-          <button className={styles.guestButton} onClick={handleGuest}>
-            게스트 로그인
-          </button>
+        <div className={styles.mainImg}>
+          <img src={main_img} alt="main" />
         </div>
       </div>
     </div>
